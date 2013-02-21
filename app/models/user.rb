ce1a95afba19d139e86d6088611e43910e27889e
Subrata <subrata@subrata-desktop.(none)>
2013-02-22 02:08:36 +0530

    facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :avatar,:email, :password, :password_confirmation, :remember_me,  :username, :age, :sex, :base_location, :real_name,:user,:user_file_name,:user_content_type,:user_file_size
+  attr_accessible :provider, :uid, :avatar,:email, :password, :password_confirmation, :remember_me,  :username, :age, :sex, :base_location, :real_name,:user,:user_file_name,:user_content_type,:user_file_size
   # attr_accessible :title, :body
   has_attached_file :avatar,
     :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
       :url => "/system/:attachment/:id/:style/:filename",
     :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+  has_many :events
+  devise :omniauthable, :omniauth_providers => [:facebook]
  
 end
