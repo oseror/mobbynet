76d03657b2921d0057bee31a8347dba484b2fe21
mac <mac@oseror.com>
2013-02-24 02:35:01 +0530

    Fix UI issue on homepage slider.

--- app/assets/javascripts/mobbynet.js
+++ app/assets/javascripts/mobbynet.js
@@ -1,19 +1,19 @@
-//$(function() {
-//  $('#da-slider').cslider({
-//    autoplay    : true, // auto play slider on load
-//    bgincrement : 450
-//  });
-//
-//  $("#src_txt").live("blur", function(){
-//    var default_value = $(this).attr("rel");
-//    if ($(this).val() == ""){
-//      $(this).val(default_value);
-//    }
-//  }).live("focus", function(){
-//    var default_value = $(this).attr("rel");
-//    if ($(this).val() == default_value){
-//      $(this).val("");
-//    }
-//  });
-//
-//});
+$(function() {
+  $('#da-slider').cslider({
+    autoplay    : true, // auto play slider on load
+    bgincrement : 450
+  });
+
+  $("#src_txt").live("blur", function(){
+    var default_value = $(this).attr("rel");
+    if ($(this).val() == ""){
+      $(this).val(default_value);
+    }
+  }).live("focus", function(){
+    var default_value = $(this).attr("rel");
+    if ($(this).val() == default_value){
+      $(this).val("");
+    }
+  });
+
+});
